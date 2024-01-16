14e40409fd0e7d08a553f307786a8d33a45ac163
jatoni <jat_antonio_torres.juan@outlook.com>
2024-01-16 15:22:47 -0600

    Feat :sparkles: Master | Se agrego el menu que se cierra y se abre, y el menu ya esta fijo, solo falta darle mas responsabilidad

--- src/main/webapp/resources/js/index.js
+++ src/main/webapp/resources/js/index.js
@@ -1,19 +1,36 @@
 const colapsarMenu = () => {
 	const contenedorMenu = document.getElementById("contenedorMenu");
+	const ocultar = document.getElementById("ocultar");
 	const card = document.querySelector(".card");
+
 	if (contenedorMenu.clientWidth > 0) {
+
+		//Esto va a funcionar para hacer que el menu se cierre
 		contenedorMenu.classList.add("cerrar");
 		contenedorMenu.classList.remove("abrir");
+		ocultar.classList.add("cerrar");
+		ocultar.classList.remove("abrir");
+
+		//Esto es para que se quite
 		contenedorMenu.style.width = "0px";
+		ocultar.style.width = "0px";
+
 		card.classList.remove("opacar");
 		card.classList.add("noOpacar");
 		setTimeout(function() {
 			card.style.display = "none"
 		}, 1000);
+
 	} else if (contenedorMenu.style.width <= "0px") {
 		contenedorMenu.classList.remove("cerrar");
 		contenedorMenu.classList.add("abrir");
-		contenedorMenu.style.width = "300px";
+		ocultar.classList.remove("cerrar");
+		ocultar.classList.add("abrir");
+
+
+		contenedorMenu.style.width = "17%";
+		ocultar.style.width = "20%";
+
 		card.classList.add("opacar");
 		card.classList.remove("noOpacar");
 		setTimeout(function() {
